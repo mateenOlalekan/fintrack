59db52d8a85ae34aa381f844c105e97ed075224a
mateenOlalekan <MatinOlalek@gmail.com>
2025-08-08 10:32:24 +0100

    completed test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,42 @@
 'use client';
+
+import { useState, useEffect } from 'react';
 import LoadingScreen from './components/loading';
-import {useState,useEffect} from 'react';
 
 import Sidebar from './components/layout/sidebar';
 import Main from './components/layout/main';
 import Tail from './components/Transactions/HomeTable';
+import Header from './components/layout/header';
 
 export default function FullLayout() {
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-        setTimeout(() => {
-            setIsLoading(false);
-        }, 2000);
-    }, []);
-    if (isLoading) {
-        return <LoadingScreen />;
-    }
-  return (
-    <div className="flex flex-col md:flex-row min-h-screen bg-gray-50">
-      {/* Sidebar */}
-      <aside className="w-full md:w-1/4 lg:w-1/5 xl:w-1/6 hidden md:block ">
-        <Sidebar />
-      </aside>
+  const [isLoading, setIsLoading] = useState(true);
 
-      {/* Main Content */}
-      <main className="flex-1 w-full overflow-y-auto">
-        <div className="p-4 space-y-6">
-          <Main />
-          <Tail />
-        </div>
-      </main>
+  useEffect(() => {
+    setTimeout(() => setIsLoading(false), 2000);
+  }, []);
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
+  return (
+    <div className="min-h-screen overflow-hidden">
+
+
+      <div className="flex pt-1 h-full">
+        {/* Sidebar */}
+        <aside className="hidden md:block w-1/4 lg:w-1/5 xl:w-1/6 bg-white shadow h-[calc(100vh-4rem)]">
+          <Sidebar />
+        </aside>
+
+        {/* Main scrollable content */}
+        <main className="flex-1   p-4 max-md:p-2 space-y-6">
+  <Main />
+  <Tail />
+</main>
+
+
+      </div>
     </div>
   );
 }
