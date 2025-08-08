46fade99209802a81ecf57b94f6d787dfea3ba06
Bayorblack Abdul-Matin Olalekan <151918649+mateenOlalekan@users.noreply.github.com>
2025-08-08 16:05:56 +0100

    Update TransactionRow.tsx

--- src/app/components/Transactions/TransactionRow.tsx
+++ src/app/components/Transactions/TransactionRow.tsx
@@ -10,19 +10,19 @@ interface Props {
 export default function TransactionRow({ transaction, isLast }: Props) {
   return (
     <tr className={!isLast ? 'border-b-2 border-[#EAF0F0] text-sm' : ''}>
-      <td className="p-3 w-[60%] whitespace-nowrap">{transaction.date}</td>
+      <td className="p-3 max-md:p-1 w-[60%] whitespace-nowrap">{transaction.date}</td>
 
-      <td className="p-3 w-[15%]">{transaction.remark}</td>
+      <td className="p-3  max-md:p-1 w-[15%]">{transaction.remark}</td>
 
-      <td className="p-3 w-[5%]">
+      <td className="p-3 max-md:p-1 w-[5%]">
         {transaction.amount < 0
           ? `-$${Math.abs(transaction.amount)}`
           : `$${transaction.amount}`}
       </td>
 
-      <td className="p-3 w-[10%]">{transaction.currency}</td>
+      <td className="p-3 max-md:p-1 w-[10%]">{transaction.currency}</td>
 
-      <td className="p-3 w-[10%]">
+      <td className="p-3 max-md:p-1 w-[10%]">
         <div className="flex items-center gap-2">
           <span
             className={`h-2 w-2 rounded-full ${
